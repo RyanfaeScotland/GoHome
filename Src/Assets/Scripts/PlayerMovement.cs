49f2700ee274c316fa936225fa2f2005bed9b16a
RyanfaeScotland <ryanfaescotland@gmail.com>
2015-06-06 23:20:15 +0100

    Committed at the end of Part 3.
    Committed at the end of Part 3 after Austin has shown us how to detect
    collisions, respawn our player object, create a particle system and
    added a class to destroy objects.

--- Src/Assets/Scripts/PlayerMovement.cs
+++ Src/Assets/Scripts/PlayerMovement.cs
@@ -3,21 +3,32 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
     public float moveSpeed;
+    public GameObject deathParticles; 
     private float maxSpeed = 5f;
     private Vector3 input;
+
+    private Vector3 spawn;
+
 	// Use this for initialization
 	void Start () {
-	
+        spawn = transform.position;
 	}
 	
 	// Update is called once per frame
-	void Update () {
+    void Update()
+    {
         input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
         if (rigidbody.velocity.magnitude < maxSpeed)
         {
             rigidbody.AddForce(input * moveSpeed);
         }
-        
-        
-	}
+    }
+
+    void OnCollisionEnter(Collision other){
+        if (other.transform.tag == "Enemy")
+        {
+            Instantiate(deathParticles, transform.position, Quaternion.identity);
+            transform.position = spawn;
+        }
+    }    
 }
